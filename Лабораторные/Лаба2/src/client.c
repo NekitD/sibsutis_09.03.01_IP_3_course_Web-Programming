e44447040e8fcf8374343033606d628eca2ca3b2
NekitD <nilitadorogin@gmail.com>
2026-02-24 20:01:57 +0700

    Lab2 first draft

---
+++ Лабораторные/Лаба2/src/client.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h> 
+
+#define BUFF_LEN 81
+
+int main()
+{
+    double i = 0;
+    int client_socket = 0;
+    char hostname[80];
+    int port = 0;
+    char msg[BUFF_LEN] = "";
+    char s_msg[BUFF_LEN] = "";
+
+    struct sockaddr_in client_addr;
+    struct sockaddr_in server_addr;
+    struct hostent *hp, *gethostbyname();
+
+    //---------------------------------------------------------------------
+    
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(client_socket < 0){
+        printf("CLIENT SOCKET CREATION FAILED!\n");
+        return -1;
+    }
+
+    bzero((char*)&client_addr, (sizeof(struct sockaddr_in)));
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    client_addr.sin_port = 0;
+    
+    if(bind(client_socket, &client_addr, sizeof(struct sockaddr_in)) < 0){
+        printf("CLIENT SOCKET INIT FAILED!\n");
+        return -1;
+    }
+    //---------------------------------------------------------------------
+    printf("HELLO USER! LET'S HAVE SOME FUN \n");
+    printf("Input some value: ");
+    scanf("%lf", &i);
+    sprintf(msg, "%.2f", i);
+    printf("\nInput hostname (no spaces): ");
+    scanf("%s", &hostname);
+    printf("\nInput port: ");
+    scanf("%d", &port);
+    //---------------------------------------------------------------------
+
+    bzero((char*)&server_addr, (sizeof(struct sockaddr_in)));
+    server_addr.sin_family = AF_INET;
+    hp = gethostbyname(hostname);
+    if (hp == NULL) {
+        printf("Cannot resolve hostname: %s\n", hostname);
+        close(client_socket);
+        return -1;
+    }
+    bcopy(hp->h_addr, &server_addr.sin_addr, hp->h_length) ;
+    server_addr.sin_port = htons(port);
+
+    if (connect(client_socket, &server_addr, sizeof(struct sockaddr_in)) < 0) {
+        printf("CONNECTION TO SERVER FAILED!\n");
+        return -1;
+    }
+
+    //---------------------------------------------------------------------
+    int length = 0;
+    int counter;
+    for(counter = 0; counter < i; counter++){
+        if(send(client_socket, msg, BUFF_LEN, 0) < 0)
+            {
+                printf("SEND TO SERVER FAILED!\n");
+                break;
+            }
+        printf("%.2f was sent.\n", i);
+
+        length = sizeof(server_addr) ;
+        bzero(s_msg, sizeof(BUFF_LEN) );
+        if ((recv(client_socket, s_msg, BUFF_LEN, 0) ) < 0)
+        { 
+            printf("Invalid server socket.\n");
+            break;
+        } else {
+            printf( "Server answered -  %s\n", s_msg);
+        }
+        sleep(i);
+    }
+
+    close(client_socket);
+    return 0;
+}
---
+++ Лабораторные/Лаба2/src/server.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h> 
+#include <arpa/inet.h>
+
+#define BUFF_LEN 81
+
+int main()
+{
+    double i = 0;
+    int server_socket = 0;
+    int socket_for_client;
+
+    struct sockaddr_in server_addr;
+
+    char msg[BUFF_LEN] = "";
+    int msgLength = 0;
+
+    //--------------------------------------------------------
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(server_socket < 0){
+        printf("SERVER SOCKET CREATION FAILED!\n");
+        return -1;
+    }
+
+    bzero((char*)&server_addr, sizeof(struct sockaddr_in));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = 0;
+    
+    if(bind(server_socket, &server_addr, sizeof(struct sockaddr_in)) < 0){
+        printf("SERVER SOCKET INIT FAILED!\n");
+        return -1;
+    }
+    //------------------------------------------------------------
+    int length = 0;
+    length = sizeof(server_addr);
+    if (getsockname(server_socket, &server_addr, &length ))
+    {
+        printf("FAILED TO GET THE SERVER PORT!\n");
+        return -1;
+    }
+    printf("SERVER: port - % d\n\n", ntohs(server_addr.sin_port ));
+    if (listen(server_socket, 5) < 0){
+        printf("LISTENING BEGIN FAILED");
+        return -1;
+    }
+
+    char ans_b[BUFF_LEN] = "SERVER: I received - ";
+    char answer[BUFF_LEN] = "";
+    for( ; ; ) {
+        socket_for_client = accept(server_socket, 0, 0);
+        if (socket_for_client < 0) {
+            printf("ACCEPT FAILED");
+        }
+        length = sizeof(socket_for_client) ;
+        for( ; ; ) {
+            bzero(msg, sizeof(BUFF_LEN));
+            bzero(answer, sizeof(BUFF_LEN));
+            if ( (msgLength = recv(socket_for_client, msg, BUFF_LEN, 0) ) < 0 )
+            { 
+                printf("Invalid client socket.\n");
+                break;
+            }
+            strcat(answer, ans_b);
+            strcat(answer, msg);
+            printf("SERVER: socket for client - %d\n", socket_for_client) ;
+            printf("SERVER: message length - %d\n", msgLength);
+            printf("SERVER: message - %s\n\n", msg);
+            send(socket_for_client, answer, BUFF_LEN, 0);
+        }
+        close(socket_for_client);
+    }
+    close(server_socket);
+    return 0;
+}
