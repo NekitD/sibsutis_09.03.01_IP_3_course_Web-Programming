3d48489ba02e3d76f5d3a78d1e33a9700b1165c3
NekitD <nilitadorogin@gmail.com>
2026-03-28 12:50:05 +0700

    Makefile rgr 1 draft

---
+++ РГР/src/web/server.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h> 
+#include "game.h"
+
+using namespace std;
+
+int main()
+{
+    
+}
